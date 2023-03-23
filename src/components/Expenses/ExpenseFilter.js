1d96485fde4965fda886079cc283a682764eb69b
Yvonne Tou <tou.yvonne@gmail.com>
2023-03-23 22:37:59 +0900

    create filter functionality, now it is able to filter by year

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -13,7 +13,8 @@ function expenseFilter(props) {
       <div className="filter-title">
         <p>Filter by year</p>
         <select className='dropdown-year' onChange={yearChangeHandler} value={props.selectedDefault}>
-          <option value='2025'>2025</option>
+          <option value='All'>All</option>
+          {/* <option value='2025'>2025</option> */}
           <option value='2024'>2024</option>
           <option value='2023'>2023</option>
           <option value='2022'>2022</option>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,22 +4,35 @@ import ExpenseItem from "./ExpenseItem";
 import ExpenseFilter from './ExpenseFilter';
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState('2023');
+  const [filteredYear, setFilteredYear] = useState('All');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
     }
 
+  let filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+    });
+  if (filteredYear === 'All') {filteredExpenses = props.items;}
+
+  // Outputting conditional content
+  // Add a if statement in && in the return statement could work too
+  // filteredExpenses.length === 0 && <p>message</p>
+  // However, this seems to be a cleaner way to handle this case
+  let expensesDisplay = <p>No expenses found in this year.</p>;
+  if (filteredExpenses.length > 0) {
+    expensesDisplay = filteredExpenses.map(expense =>    <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+      />);
+  }
   return (
     <div className='expenses-container'>
       <ExpenseFilter onFilterChange={filterChangeHandler} selectedDefault={filteredYear} />
       <div className='expenses'>
-        {props.items.map(expense => <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />)}
+        {expensesDisplay}
       </div>
     </div>
  )
