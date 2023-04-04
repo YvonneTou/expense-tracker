b26ba2ab1dbfe0e7f7ab98696e07642cb26be27f
Yvonne Tou <tou.yvonne@gmail.com>
2023-04-04 21:08:44 +0900

    remove unused useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,7 +7,7 @@ function ExpenseItem(props) {
   // in the default JS Date class, month starts from 0
   // for March, we input '2'
   // const expenseDate = new Date(2023, 2, 5);
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
   // const clickHandler = () => {
   //   setTitle('Updated!');
